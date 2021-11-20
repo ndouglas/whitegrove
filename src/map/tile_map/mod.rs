83c6941da0c2709e883ffd167dd940f25b13f5d8
Nathan Douglas <github@darkdell.net>
2021-11-20 11:49:53 -0500

    Refactor random map generation.

--- src/map/tile_map/mod.rs
+++ src/map/tile_map/mod.rs
@@ -3,27 +3,33 @@ use crate::model::xy_to_idx;
 
 use super::TileType;
 
-pub fn get_random_tile_map(width: usize, height: usize) -> Vec<TileType> {
-    let length = width * height;
-    let mut result = vec![TileType::Floor; length];
+pub fn add_borders_to_tile_map(tiles: &mut Vec<TileType>, width: usize, height: usize) {
     for x in 0..width {
-        result[xy_to_idx(width, x, 0)] = TileType::Wall;
-        result[xy_to_idx(width, x, height - 1)] = TileType::Wall;
+        tiles[xy_to_idx(width, x, 0)] = TileType::Wall;
+        tiles[xy_to_idx(width, x, height - 1)] = TileType::Wall;
     }
     for y in 0..height {
-        result[xy_to_idx(width, 0, y)] = TileType::Wall;
-        result[xy_to_idx(width, width - 1, y)] = TileType::Wall;
+        tiles[xy_to_idx(width, 0, y)] = TileType::Wall;
+        tiles[xy_to_idx(width, width - 1, y)] = TileType::Wall;
     }
+}
 
+pub fn add_random_walls_to_tile_map(tiles: &mut Vec<TileType>, width: usize, height: usize, count: usize) {
     let mut rng = rltk::RandomNumberGenerator::new();
-
-    for _i in 0..length / 10 {
+    for _i in 0..count {
         let x = rng.roll_dice(1, (width - 1).try_into().unwrap()) as usize;
         let y = rng.roll_dice(1, (height - 1).try_into().unwrap()) as usize;
         let idx = xy_to_idx(width, x, y);
         if idx != xy_to_idx(width, width / 2, height / 2) {
-            result[idx] = TileType::Wall;
+            tiles[idx] = TileType::Wall;
         }
     }
+}
+
+pub fn get_random_tile_map(width: usize, height: usize) -> Vec<TileType> {
+    let length = width * height;
+    let mut result = vec![TileType::Floor; length];
+    add_borders_to_tile_map(&mut result, width, height);
+    add_random_walls_to_tile_map(&mut result, width, height, length / 10);
     result
 }
